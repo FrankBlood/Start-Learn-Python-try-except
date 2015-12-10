494286c9ffe52f0dcb1e293df5ddf7932666864d
FrankBlood <1062947208@qq.com>
2015-12-10 11:53:47 +0800

    try finally

---
+++ test3.py
@@ -0,0 +1,12 @@
+import time
+try:
+	f=open('poem.txt')
+	while True:
+		line=f.readline()
+		if len(line)==0:
+			break
+		time.sleep(2)
+		print(line)
+finally:
+	f.close()
+	print('Cleanling up... closed the file')
